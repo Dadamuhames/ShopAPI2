ac8403c54007b0304cab873890618bb71e009fae
MSD <dadamuhames@gmail.com>
2022-11-30 19:19:23 +0500

    Diploy

--- shop/settings.py
+++ shop/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xz0mg(%jhattp$pod-kl15$c)tdkna(2avkv(egco2(o(d(aq1'
-
+#SECRET_KEY = 'django-insecure-xz0mg(%jhattp$pod-kl15$c)tdkna(2avkv(egco2(o(d(aq1'
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -43,11 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',  'easy_thumbnails',
-    'djoser',
+    'djoser', 'corsheaders',
     'rest_framework', 'rest_framework.authtoken',
     'account', 'order', 'colorfield',
     'ckeditor_uploader', 'ckeditor',
-    'django_filters', 'corsheaders',
+    'django_filters', 
 ]
 
 MIDDLEWARE = [
