4b8206cb9950ab6146daa12c68533971d1867ec4
MSD <dadamuhames@gmail.com>
2022-11-30 19:34:58 +0500

    Diploy2

--- shop/settings.py
+++ shop/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-xz0mg(%jhattp$pod-kl15$c)tdkna(2avkv(egco2(o(d(aq1'
-SECRET_KEY = os.getenv("DJANGO_SECRET")
+SECRET_KEY = 'django-insecure-xz0mg(%jhattp$pod-kl15$c)tdkna(2avkv(egco2(o(d(aq1'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
