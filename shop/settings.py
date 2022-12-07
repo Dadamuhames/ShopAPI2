27c9a31abbf02e23561eb97eb138cb3d064cbf73
MSD <dadamuhames@gmail.com>
2022-12-07 18:36:22 +0500

    Commit Wow 2

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xz0mg(%jhattp$pod-kl15$c)tdkna(2avkv(egco2(o(d(aq1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
